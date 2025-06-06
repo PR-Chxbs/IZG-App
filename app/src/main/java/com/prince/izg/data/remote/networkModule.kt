112bb8e04e78176877cd9b056e11130b134c725c
Wahina Chabalala <wp.chabalala@gmail.com>
2025-06-07 00:10:57 +0200

    feat: create NetworkModule

---
+++ app/src/main/java/com/prince/izg/data/remote/networkModule.kt
@@ -0,0 +1,55 @@
+package com.prince.izg.data.remote
+
+import com.prince.izg.data.remote.api.*
+import com.prince.izg.util.Constants
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder()
+            // No interceptor since token is passed manually
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(Constants.BASE_URL)  // e.g. "https://yourapi.com/api/"
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideUserApi(retrofit: Retrofit): UserApi = retrofit.create(UserApi::class.java)
+
+    @Provides
+    @Singleton
+    fun providePostApi(retrofit: Retrofit): PostApi = retrofit.create(PostApi::class.java)
+
+    @Provides
+    @Singleton
+    fun provideCategoryApi(retrofit: Retrofit): CategoryApi = retrofit.create(CategoryApi::class.java)
+
+    @Provides
+    @Singleton
+    fun provideEventApi(retrofit: Retrofit): EventApi = retrofit.create(EventApi::class.java)
+
+    @Provides
+    @Singleton
+    fun provideStockApi(retrofit: Retrofit): StockApi = retrofit.create(StockApi::class.java)
+}
---
+++ app/src/main/java/com/prince/izg/util/Constants.kt
@@ -0,0 +1,5 @@
+package com.prince.izg.util
+
+object Constants {
+    const val BASE_URL = "https://izg-backend.onrender.com/api" // Backend server URL
+}
