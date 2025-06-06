4d40751e9839274c05d3f8c437e78647308cc0f3
Wahina Chabalala <wp.chabalala@gmail.com>
2025-06-06 23:35:20 +0200

    feat: create Post Api
    Post Api
    
    /posts
    
    - post: /
    - get: /
    - get: /:id
    - put: /:id
    - delete: /:id
    - put: /publish/:id
    - put: /unpublish/:id

---
+++ app/src/main/java/com/prince/izg/data/remote/api/PostApi.kt
@@ -0,0 +1,48 @@
+package com.prince.izg.data.remote.api
+
+import com.prince.izg.data.remote.dto.Post.PostRequest
+import com.prince.izg.data.remote.dto.Post.PostResponse
+import retrofit2.Response
+import retrofit2.http.*
+
+interface PostApi {
+
+    @POST("/posts")
+    suspend fun addPost(
+        @Header("Authorization") token: String,
+        @Body post: PostRequest
+    ): Response<PostResponse>
+
+    @GET("/posts")
+    suspend fun getPosts(): Response<List<PostResponse>>
+
+    @GET("/posts/{id}")
+    suspend fun getPostById(
+        @Path("id") id: Int
+    ): Response<PostResponse>
+
+    @PUT("/posts/{id}")
+    suspend fun updatePostById(
+        @Header("Authorization") token: String,
+        @Path("id") id: Int,
+        @Body post: PostRequest
+    ): Response<PostResponse>
+
+    @DELETE("/posts/{id}")
+    suspend fun deletePostById(
+        @Header("Authorization") token: String,
+        @Path("id") id: Int
+    ): Response<PostResponse>
+
+    @PUT("/posts/publish/{id}")
+    suspend fun publishPostById(
+        @Header("Authorization") token: String,
+        @Path("id") id: Int
+    ): Response<PostResponse>
+
+    @PUT("/posts/unpublish/{id}")
+    suspend fun unpublishPostById(
+        @Header("Authorization") token: String,
+        @Path("id") id: Int
+    ): Response<PostResponse>
+}
