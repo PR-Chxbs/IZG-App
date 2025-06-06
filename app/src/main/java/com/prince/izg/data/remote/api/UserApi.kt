d453bbea51cd07bf5a51ef57de2117ee76554b1f
Wahina Chabalala <wp.chabalala@gmail.com>
2025-06-06 23:33:13 +0200

    feat: create UserApi
    User Api
    
    /users
    
    - post: /
    - get: /
    - get: /:id
    - put: /:id
    - delete: /:id

---
+++ app/src/main/java/com/prince/izg/data/remote/api/UserApi.kt
@@ -0,0 +1,39 @@
+package com.prince.izg.data.remote.api
+
+import com.prince.izg.data.remote.dto.User.UserRequest
+import com.prince.izg.data.remote.dto.User.UserResponse
+import retrofit2.Response
+import retrofit2.http.*
+
+interface UserApi {
+
+    @POST("/users")
+    suspend fun addUser(
+        @Header("Authorization") token: String,
+        @Body user: UserRequest
+    ): Response<UserResponse>
+
+    @GET("/users")
+    suspend fun getUsers(
+        @Header("Authorization") token: String
+    ): Response<List<UserResponse>>
+
+    @GET("/users/{id}")
+    suspend fun getUserById(
+        @Header("Authorization") token: String,
+        @Path("id") id: Int
+    ): Response<UserResponse>
+
+    @PUT("/users/{id}")
+    suspend fun updateUserById(
+        @Header("Authorization") token: String,
+        @Path("id") id: Int,
+        @Body user: UserRequest
+    ): Response<UserResponse>
+
+    @DELETE("/users/{id}")
+    suspend fun deleteUserById(
+        @Header("Authorization") token: String,
+        @Path("id") id: Int
+    ): Response<UserResponse>
+}
