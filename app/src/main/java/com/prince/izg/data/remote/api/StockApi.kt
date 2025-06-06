c1ce5bc87441625506119b8a0f24c20aa1ba904e
Wahina Chabalala <wp.chabalala@gmail.com>
2025-06-06 23:33:58 +0200

    feat: create Stock Api
    Stock Api
    
    /stock
    
    - post: /
    - get: /
    - get: /:id
    - put: /:id
    - delete: /:id

---
+++ app/src/main/java/com/prince/izg/data/remote/api/StockApi.kt
@@ -0,0 +1,36 @@
+package com.prince.izg.data.remote.api
+
+import com.prince.izg.data.remote.dto.Stock.StockRequest
+import com.prince.izg.data.remote.dto.Stock.StockResponse
+import retrofit2.Response
+import retrofit2.http.*
+
+interface StockApi {
+
+    @POST("/stock")
+    suspend fun addStock(
+        @Header("Authorization") token: String,
+        @Body stock: StockRequest
+    ): Response<StockResponse>
+
+    @GET("/stock")
+    suspend fun getStockItems(): Response<List<StockResponse>>
+
+    @GET("/stock/{id}")
+    suspend fun getStockItemById(
+        @Path("id") id: Int
+    ): Response<StockResponse>
+
+    @PUT("/stock/{id}")
+    suspend fun updateStockItemById(
+        @Header("Authorization") token: String,
+        @Path("id") id: Int,
+        @Body stock: StockRequest
+    ): Response<StockResponse>
+
+    @DELETE("/stock/{id}")
+    suspend fun deleteStockItemById(
+        @Header("Authorization") token: String,
+        @Path("id") id: Int
+    ): Response<StockResponse>
+}
