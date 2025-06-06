69c3b9a4d641a04d652f71d6ff01dccd9feff582
Wahina Chabalala <wp.chabalala@gmail.com>
2025-06-06 21:52:55 +0200

    chore: add retrofit2
    Added retrofit2
    
    - App: build.gradle
    - libs.versions.toml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,4 +56,9 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    // ----------- PR Added ----------- //
+    // Retrofit 2
+    implementation(libs.retrofit)
+    implementation(libs.retrofit.gson) // If you're using Gson
 }
