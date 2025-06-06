0f1e99028e6ef65178939fb53fc1e6e629f5dc45
Wahina Chabalala <wp.chabalala@gmail.com>
2025-06-06 22:00:45 +0200

    feat: create AuthApi
    Auth Api
    
    /auth
    
    - /login
    - /register

---
+++ app/src/main/java/com/prince/izg/data/remote/api/AuthApi.kt
@@ -0,0 +1,22 @@
+package com.prince.izg.data.remote.api
+
+import com.prince.izg.data.remote.dto.Auth.LoginRequest
+import com.prince.izg.data.remote.dto.Auth.RegisterRequest
+import com.prince.izg.data.remote.dto.Auth.LoginResponse
+import com.prince.izg.data.remote.dto.Auth.RegisterResponse
+import retrofit2.http.Body
+import retrofit2.http.POST
+
+
+interface AuthApi {
+
+    @POST("/auth/login")
+    suspend fun login(
+        @Body request: LoginRequest
+    ): LoginResponse
+
+    @POST("/auth/register")
+    suspend fun register(
+        @Body request: RegisterRequest
+    ): RegisterResponse
+}
