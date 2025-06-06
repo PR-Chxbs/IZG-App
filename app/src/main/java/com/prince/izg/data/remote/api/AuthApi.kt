57396e8d7fb390ac42949d7db3e3f324e3a37913
Wahina Chabalala <wp.chabalala@gmail.com>
2025-06-06 23:37:27 +0200

    Update AuthApi.kt

--- app/src/main/java/com/prince/izg/data/remote/api/AuthApi.kt
+++ app/src/main/java/com/prince/izg/data/remote/api/AuthApi.kt
@@ -4,6 +4,7 @@ import com.prince.izg.data.remote.dto.Auth.LoginRequest
 import com.prince.izg.data.remote.dto.Auth.RegisterRequest
 import com.prince.izg.data.remote.dto.Auth.LoginResponse
 import com.prince.izg.data.remote.dto.Auth.RegisterResponse
+import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.POST
 
@@ -13,10 +14,10 @@ interface AuthApi {
     @POST("/auth/login")
     suspend fun login(
         @Body request: LoginRequest
-    ): LoginResponse
+    ): Response<LoginResponse>
 
     @POST("/auth/register")
     suspend fun register(
         @Body request: RegisterRequest
-    ): RegisterResponse
+    ): Response<RegisterResponse>
 }
