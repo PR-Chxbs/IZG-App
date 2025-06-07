573ac16bdf96126b38c558f341e7a33d8b93c805
Wahina Chabalala <wp.chabalala@gmail.com>
2025-06-07 16:17:28 +0200

    feat: create UserRepository

---
+++ app/src/main/java/com/prince/izg/data/repository/UserRepository.kt
@@ -0,0 +1,29 @@
+package com.prince.izg.data.repository
+
+import com.prince.izg.data.remote.api.UserApi
+import com.prince.izg.data.remote.dto.User.UserRequest
+import com.prince.izg.data.remote.dto.User.UserResponse
+
+class UserRepository(
+    private val userApi: UserApi
+) {
+    suspend fun addUser(token: String, user: UserRequest): UserResponse {
+        return userApi.addUser("Bearer $token", user)
+    }
+
+    suspend fun getUsers(token: String): List<UserResponse> {
+        return userApi.getUsers("Bearer $token")
+    }
+
+    suspend fun getUserById(token: String, id: Int): UserResponse {
+        return userApi.getUserById("Bearer $token", id)
+    }
+
+    suspend fun updateUserById(token: String, id: Int, user: UserRequest): UserResponse {
+        return userApi.updateUserById("Bearer $token", id, user)
+    }
+
+    suspend fun deleteUserById(token: String, id: Int): UserResponse {
+        return userApi.deleteUserById("Bearer $token", id)
+    }
+}
