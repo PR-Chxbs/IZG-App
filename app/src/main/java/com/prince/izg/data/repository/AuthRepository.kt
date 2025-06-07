b382e7ede64190fdb7bfe8f23bfd6eea1b8f09c9
Wahina Chabalala <wp.chabalala@gmail.com>
2025-06-07 16:29:51 +0200

    feat: create AuthRepository

---
+++ app/src/main/java/com/prince/izg/data/repository/AuthRepository.kt
@@ -0,0 +1,21 @@
+package com.prince.izg.data.repository
+
+import com.prince.izg.data.remote.api.AuthApi
+import com.prince.izg.data.remote.dto.Auth.LoginRequest
+import com.prince.izg.data.remote.dto.Auth.LoginResponse
+import com.prince.izg.data.remote.dto.Auth.RegisterRequest
+import com.prince.izg.data.remote.dto.Auth.RegisterResponse
+import javax.inject.Inject
+
+class AuthRepository @Inject constructor(
+    private val authApi: AuthApi
+) {
+
+    suspend fun registerUser(registerRequest: RegisterRequest): RegisterResponse {
+        return authApi.register(registerRequest)
+    }
+
+    suspend fun loginUser(loginRequest: LoginRequest): LoginResponse {
+        return authApi.login(loginRequest)
+    }
+}
