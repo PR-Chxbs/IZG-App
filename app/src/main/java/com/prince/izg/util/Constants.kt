907119c85083ec7a626b869abe8230591386ef79
Wahina Chabalala <wp.chabalala@gmail.com>
2025-06-08 09:28:36 +0200

    feat: add datastore name to consts

--- app/src/main/java/com/prince/izg/util/Constants.kt
+++
@@ -1,5 +0,0 @@
-package com.prince.izg.util
-
-object Constants {
-    const val BASE_URL = "https://izg-backend.onrender.com/api" // Backend server URL
-}
---
+++ app/src/main/java/com/prince/izg/utils/Constants.kt
@@ -0,0 +1,6 @@
+package com.prince.izg.utils
+
+object Constants {
+    const val BASE_URL = "https://izg-backend.onrender.com/api"
+    const val DATASTORE_NAME = "izg_prefs"
+}
