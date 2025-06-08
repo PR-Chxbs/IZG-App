e78f19a3a9fd21f761ca2317ad7d0ba62acf6357
Wahina Chabalala <wp.chabalala@gmail.com>
2025-06-08 09:26:51 +0200

    feat: create DataStoreModule

---
+++ app/src/main/java/com/prince/izg/di/DataStoreModule.kt
@@ -0,0 +1,28 @@
+package com.prince.izg.di
+
+import android.content.Context
+import androidx.datastore.preferences.preferencesDataStore
+import com.prince.izg.utils.Constants
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStoreFile
+import androidx.datastore.preferences.core.PreferenceDataStoreFactory
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DataStoreModule {
+
+    @Provides
+    @Singleton
+    fun provideDataStore(@ApplicationContext context: Context): DataStore<Preferences> {
+        return PreferenceDataStoreFactory.create {
+            context.preferencesDataStoreFile(Constants.DATASTORE_NAME)
+        }
+    }
+}
